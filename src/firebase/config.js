5c866fb1506cf81b46c5ae765efece7910b0fe40
Alejandra Gonzales <69376514+alegita@users.noreply.github.com>
2020-08-14 21:43:50 -0400

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDESQSgEt5hnCAcghDOd38bupLexqxHVcA",
+    authDomain: "newsy-13f96.firebaseapp.com",
+    databaseURL: "https://newsy-13f96.firebaseio.com",
+    projectId: "newsy-13f96",
+    storageBucket: "newsy-13f96.appspot.com",
+    messagingSenderId: "466231935137",
+    appId: "1:466231935137:web:3dac1162a975ec198eb0e0"
+  };
+
+  export default firebaseConfig;
